ee49abcb9b7de73eb65064afdf35f111f43fa4e2
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-05 09:04:51 -0300

    build(orm): config prisma & prisma client

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PrismaModule } from "./prisma/prisma.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
