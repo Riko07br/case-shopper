b57eb3eacba621ca5124e8531dab1954fa874332
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-05 15:15:33 -0300

    build: add file upload handler & csv parser

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ConfigModule } from "@nestjs/config";
+import { ProductsModule } from "./products/products.module";
+import { MulterModule } from "@nestjs/platform-express";
 
 @Module({
-    imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+    imports: [
+        PrismaModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        ProductsModule,
+        MulterModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
