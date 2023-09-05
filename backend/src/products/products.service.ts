30026c738eed406778fb1a3f7036536855036fbe
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-05 17:18:15 -0300

    feat(product): add more validation layers

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import {
+    BadRequestException,
+    HttpException,
+    HttpStatus,
+    Injectable,
+} from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { Readable } from "stream";
 import * as papa from "papaparse";
@@ -16,7 +21,6 @@ export class ProductsService {
                 delimiter: ",",
                 worker: true,
                 complete: function (results) {
-                    //this.validadeFile(results);
                     resolve(results.data);
                 },
                 error(err) {
@@ -29,36 +33,114 @@ export class ProductsService {
     async validadeFile(results: Array<Array<string | number>>) {
         // validar campos (headers)
         if (results[0][0] != "product_code" || results[0][1] != "new_price") {
+            throw new HttpException(
+                [
+                    {
+                        column: 0,
+                        line: 0,
+                        err: "Cabeçalho inválido, utilize 'product_code' e 'new_price' respectivamente",
+                    },
+                ],
+                HttpStatus.UNPROCESSABLE_ENTITY
+            );
         }
 
         let errorObj: { column: number; line: number; err?: string }[] = [];
 
-        // Os códigos de produtos informados existem?
         for (let i = 1; i < results.length; i++) {
-            for (let j = 0; j < 2; j++) {
-                //caso o array nao esteja completo
-                if (results[i][j] == undefined) {
-                    errorObj.push({ column: j, line: i, err: "Posição vazia" });
-                    continue;
-                }
-                const productCode: number = Number(results[i][0]);
-                const product = await this.prisma.products.findFirst({
-                    where: { code: productCode },
+            //caso o array nao esteja completo ou não tenha números
+            if (results[i][0] == undefined || Number.isNaN(results[i][0])) {
+                errorObj.push({
+                    column: 0,
+                    line: i,
+                    err: "Código do produto não é um número válido",
                 });
-
-                if (!product) {
-                    errorObj.push({
-                        column: j,
-                        line: i,
-                        err: "Produto inexistente",
-                    });
-                }
+                continue;
             }
-        }
-        console.log(errorObj);
-        //Os preços estão preenchidos e são valores numéricos validos?
 
-        //O arquivo respeita as regras levantadas na seção CENARIO?
+            if (results[i][1] == undefined || Number.isNaN(results[i][1])) {
+                errorObj.push({
+                    column: 1,
+                    line: i,
+                    err: "Preço do produto não é um número válido",
+                });
+                continue;
+            }
+
+            //o código precisa ser inteiro
+            const productCode: number = Number(results[i][0]);
+            if (!Number.isInteger(productCode)) {
+                errorObj.push({
+                    column: 0,
+                    line: i,
+                    err: "Código do produto precisa ser um número inteiro",
+                });
+                continue;
+            }
+
+            const newPrice: number = Number(results[i][1]);
+
+            //procura no banco de dados
+            const product = await this.prisma.products.findFirst({
+                where: { code: productCode },
+            });
+
+            //caso o produto não exista
+            if (!product) {
+                errorObj.push({
+                    column: 0,
+                    line: i,
+                    err: "Produto inexistente",
+                });
+                continue;
+            }
+            //Impede preços de venda menores que o custo
+            const dbCostPrice: number = product.cost_price.toNumber();
+
+            if (newPrice < dbCostPrice) {
+                errorObj.push({
+                    column: 1,
+                    line: i,
+                    err:
+                        "Preço de venda R$ " +
+                        newPrice.toFixed(2) +
+                        " menor que o custo R$ " +
+                        dbCostPrice.toFixed(2),
+                });
+                continue;
+            }
+
+            //Impede variações maior que 10% do valor salvo
+            const dbSalesPrice: number = product.sales_price.toNumber();
+
+            if (
+                newPrice > dbSalesPrice * 1.1 ||
+                newPrice < dbSalesPrice * 0.9
+            ) {
+                errorObj.push({
+                    column: 1,
+                    line: i,
+                    err:
+                        "Variação de preço maior que 10%, min: R$ " +
+                        (dbSalesPrice * 0.9).toFixed(2) +
+                        " max: R$ " +
+                        (dbSalesPrice * 1.1).toFixed(2) +
+                        " utilizado: R$ " +
+                        newPrice.toFixed(2),
+                });
+                continue;
+            }
+
+            /*TODO: Estabeleceu-se a regra que, ao reajustar o preço de um pacote, o mesmo arquivo deve
+             * conter os reajustes dos preços dos componentes do pacote de modo que o preço final da
+             * soma dos componentes seja igual ao preço do pacote.
+             * O preço de custo dos pacotes também deve ser atualizado como a soma dos
+             * custos dos seus componentes
+             */
+        }
+
+        if (errorObj.length > 0)
+            throw new HttpException(errorObj, HttpStatus.UNPROCESSABLE_ENTITY);
 
         return results;
     }
