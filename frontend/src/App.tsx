7cb5dccc0988cae7600c9ecdcd0f6aff72da3e2a
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-07 00:30:38 -0300

    feat: frontend can make validation requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,44 @@
-//import { useState } from 'react'
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {}
+function App() {
+    const [selectedFile, setSelectedFile] = useState<File>();
+    // usar state para salvar a resposta valida
+    //const [validProducts, setValidProducts] = useState([]);
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setSelectedFile(e.target.files[0]);
+        }
+    };
+
+    const handleValidationClick = () => {
+        if (!selectedFile) {
+            return;
+        }
+
+        axios
+            .postForm(import.meta.env.VITE_API_URL + "upload", {
+                csv_file: selectedFile,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+
+            <div>
+                <button onClick={handleValidationClick}>Validar</button>
+            </div>
+        </div>
+    );
+}
 
 export default App;
