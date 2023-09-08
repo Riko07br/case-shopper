76dd18b27895a60dc977ceac6854fe97d560fde1
Natanael Felix <f.natanael.felix.b@gmail.com>
2023-09-07 22:43:55 -0300

    style: decimals to prices

--- frontend/src/Products.tsx
+++ frontend/src/Products.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Products = ({ product: { code, name, oldPrice, newPrice, err } }) => {
+    const parsedNewPrice: string = (newPrice as number).toFixed(2);
+    const parsedOldPrice: string = (oldPrice as number).toFixed(2);
     return (
-        <tr>
+        <tr className={err ? "productError" : ""}>
             <td>{code}</td>
             <td>{name}</td>
-            <td>{oldPrice}</td>
-            <td>{newPrice}</td>
+            <td>R$ {parsedOldPrice}</td>
+            <td>R$ {parsedNewPrice}</td>
             <td>{err ? err : "Nenhum"}</td>
         </tr>
     );
